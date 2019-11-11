35820cf266569010e2439b228896e9ea43fbc61d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-11-11 12:21:57 +0100

    BC gemaakt en apod

--- src/components/apod.jsx
+++ src/components/apod.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+// eslint-disable-next-line
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import styles from './styles.css'
 
 class Apod extends React.Component {
 
     constructor(props) {
         super(props)
-
+        this.state = {
+            apod_image: ""
+        }
         this.apod = this.apod.bind(this);
     }
 
     apod() {
-        fetch("https://api.nasa.gov/planetary/apod?api_key=nbw6CmJP3hcYinsbrg60vfpTU1us0UR3mB67ayP6")
+        fetch("https://api.nasa.gov/planetary/apod?api_key=6UwMTfVZC88kYd5c1Zr4ixXoeaK0aVeKl92hbYJq")
         .then((res) => {
             res.json().then((data) => {
-                console.log("Hey")
+               console.log(data.hdurl)
+               localStorage.setItem("apod",data.hdurl);
             })
         })
     }
@@ -21,7 +26,7 @@ class Apod extends React.Component {
     render() {
         return(
             <div>
-                <table>
+                <table onLoad={this.apod()}>
                     <thead className="border border-white">
                     <tr className="text-primary">
                         <th>Astronomy Picture of the Day</th>
@@ -29,7 +34,7 @@ class Apod extends React.Component {
                     </thead>
                     <tbody className="border border-white">
                     <tr className="text-primary">
-                        <th>{this.apod()}</th>
+                        <th><img className="img-fluid" src={localStorage.getItem("apod")}/></th>
                     </tr>
                     </tbody>
                 </table>
