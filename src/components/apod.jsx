18edd0ea167a43ff7da2f80558c78995d786c1cb
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-11-11 09:31:04 +0100

    APOD functie gemaakt

--- src/components/apod.jsx
+++ src/components/apod.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
 class Apod extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.apod = this.apod.bind(this);
+    }
+
+    apod() {
+        fetch("https://api.nasa.gov/planetary/apod?api_key=nbw6CmJP3hcYinsbrg60vfpTU1us0UR3mB67ayP6")
+        .then((data) => {
+            console.log(data)
+        })
+    }
+
     render() {
         return(
             <div>
@@ -13,7 +27,7 @@ class Apod extends React.Component {
                     </thead>
                     <tbody className="border border-white">
                     <tr className="text-primary">
-                        <th>Hallo</th>
+                        <th>Hallo, {this.apod()}</th>
                     </tr>
                     </tbody>
                 </table>
