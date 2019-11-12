ffda4baa69d6e3c4f331966dedc561807b8f324a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-11-12 09:25:07 +0100

    Default class gemaakt die hey schrijft

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,2 +1,18 @@
 import  React from 'react';
 import ReactDom from 'react-dom'
+
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return(
+            <div>
+                <p>Hey</p>
+            </div>
+        )
+    }
+}
+
+export default Search
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Apod from './components/apod.jsx'
+import Search from './components/search.jsx'
 
 
 class App extends React.Component {
     render() {
         return(
+            <div>
             <Apod/>
+            <Search/>
+            </div>
         )
     }
 }
