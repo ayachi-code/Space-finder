57c563a6e5fd44e742b71adc2d05f15c3e4e6d5e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-11-12 09:38:39 +0100

    input gecenterd

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -5,11 +5,16 @@ import ReactDom from 'react-dom'
 class Search extends React.Component {
     constructor(props) {
         super(props)
+        this.input = {
+            margin: "0",
+            position: "absolute",
+            top: "50%"
+        }
     }
     render() {
         return(
-            <div>
-                <p>Hey</p>
+            <div className="w-100 text-center"  style={this.input}>
+                <input className="rounded-pill text-center " type="input" placeholder="Type een planeet"/>
             </div>
         )
     }
