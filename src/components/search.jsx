301c51ce341b8cd7ece250047bafebbbe9295c1f
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-01-29 10:42:38 +0100

    Informatie word op pagina gezet

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -20,7 +20,7 @@ class Search extends React.Component {
         this.get_planet_picture = this.get_planet_picture.bind(this);
         this.get_planet_info = this.get_planet_info.bind(this);
         this.expPlanet = this.expPlanet.bind(this);
-        this.get_exoPlanet_data = this.get_exoPlanet_data.bind(this);
+        //this.get_exoPlanet_data = this.get_exoPlanet_data.bind(this);
         this.pageload = this.pageload.bind(this);
     }
 
@@ -32,6 +32,7 @@ class Search extends React.Component {
         window.addEventListener('load',this.pageload)
     }
 
+    /*
     get_exoPlanet_data(EXO) {
         fetch("https://raw.githubusercontent.com/paulfitz/exoplanets/master/data/exoplanet.json")
             .then((res) => res.json())
@@ -59,6 +60,8 @@ class Search extends React.Component {
                  }
             } );
     }
+    */
+
 
     get_planet_picture(PlanetName) {
         //Fetched na NASA API voor de fotos
@@ -106,12 +109,23 @@ class Search extends React.Component {
             console.log(Planet_info)
             if (Planet_info.Star_name2.includes(EXO)) {
                 console.log("Er is informatie van beschikbaar")
+                let index_of_planet = Planet_info.Star_name2.indexOf(EXO);
+                
+                document.getElementById("gravity").innerText = "Name: " + data[index_of_planet]["# name"];
+                document.getElementById("sun_d").innerText = "Semi mayor axis: " + data[index_of_planet].semi_major_axis + " A.U";
+                document.getElementById("diameter").innerText = "Oribital period: " + Math.floor(data[index_of_planet].orbital_period)  + " days";
+                document.getElementById("volume").innerText = "Angular distance: " + data[index_of_planet].angular_distance + "°";
+                // eslint-disable-next-line no-useless-concat
+                document.getElementById("mass").innerText =  "Mass: " + data[index_of_planet].mass * 100 + "*" +  " Earth mass ";
+                document.getElementById("density").innerText = "Discoverd in: " + data[index_of_planet].discovered;
             } else {
                 console.log("Er is geen informatie van beschikbaar")
                 //Kijkt als er nog fotos van beschikbaar zijn.
             }
            
         })
+
+
 
 /*
         fetch("https://images-api.nasa.gov/search?q=" + EXO)
