8dfd139c9dd9585a64a037a981c9d9437a679c32
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-01-31 19:59:13 +0100

    Firebase geinitialiseerd

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Firebase from 'firebase'
 
 
 class Feedback extends React.Component {
     constructor() {
         super()
+        this.appConfig = {
+            apiKey: "AIzaSyDrKye_UsnwyGMiVsvz0YgjlyQI-5TFgu0",
+            authDomain: "space-finder-d2034.firebaseapp.com",
+            databaseURL: "https://space-finder-d2034.firebaseio.com",
+            projectId: "space-finder-d2034",
+            storageBucket: "space-finder-d2034.appspot.com",
+            messagingSenderId: "905483037640",
+            appId: "1:905483037640:web:df21520cf2925ea4eeeefd",
+            measurementId: "G-E5DBN3R0KC"
+        }
+        Firebase.initializeApp(this.appConfig)
         this.feedbackStyle = {
             position: "fixed",
             bottom: "0",
@@ -12,7 +24,8 @@ class Feedback extends React.Component {
     }
 
     report() {
-        console.log("click")
+        let feedback = prompt("Type your feedback here")
+        console.log(feedback)
     }
 
 
